fb0b17463a71d1a208a70b61f9917e27b8467531
Barta János Máté <bartamatejanos@gmail.com>
2025-09-19 12:29:34 +0200

    task 1-2 done

--- task1/task.js
+++ task1/task.js
@@ -1,3 +1,10 @@
 export function findByActorNameAndGenre(allMovies, allActors, actorName, genre) {
-
+    const actor = allActors.find(a => a.name === actorName);
+    if (!actor) {
+        return [];
+    }
+    return allMovies
+    .filter(m => m.actor_ids.includes(actor.id))
+    .filter(m => m.genres.includes(genre))
+    .map(m => m.title);
 }
--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,12 @@
 export function findUniqueGenres(genres) {
+   const unique = [];
 
+   genres.forEach(inner => {
+    inner.forEach(g => {
+        if (!unique.includes(g)) {
+            unique.push(g);
+        }
+    });
+   });
+   return unique;
 }
